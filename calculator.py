73424c50323fbe847b5c3b0d044be82840c60f1a
bigdrichards <70369636+bigdrichards@users.noreply.github.com>
2020-08-28 02:43:37 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,43 @@
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    if y == 0
+        return 'Invalid value for denominator, can't divide by 0!'
+    return x/y
 
 
 def multiply(x, y):
-    pass
+    return x*y
 
 
 def square(x):
-    pass
+    return x*x
 
 
 def power(x, y):
-    pass
+    return power(x,y)
 
 
 def sqrt(x):
-    pass
+    return sqrt(x)
+
+# For the add() function
+$ py.test test_add.py
+
+# For the subtract() function
+$ py.test test_subtract.py
+
+# For the mulitply() function
+$ py.test test_multiply.py
+
+$ py.test test_divide.py
+
+$ py.test test_square.py
+$ py.test test_power.py
+$ py.test test_sqrt.py
